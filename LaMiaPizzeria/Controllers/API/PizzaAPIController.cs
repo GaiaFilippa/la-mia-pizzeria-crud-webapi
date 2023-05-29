a769e9e38d879abb51b47e2f3e3562dee26ce45d
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-05-29 14:42:56 +0200

    added method Search By Id

--- LaMiaPizzeria/Controllers/API/PizzaAPIController.cs
+++ LaMiaPizzeria/Controllers/API/PizzaAPIController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LaMiaPizzeria.Database;
+using LaMiaPizzeria.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LaMiaPizzeria.Controllers.API
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class PizzaAPIController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetPizze()
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizze = db.Pizze.ToList();
+                return Ok(pizze);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult SearchById(int id)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza? pizzaToSearch = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if (pizzaToSearch != null)
+                {
+                    return Ok(pizzaToSearch);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+        }
+
     }
 }
